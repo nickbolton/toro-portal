93078d400dd7d4e17b6ecd2350bb5c0bb679fa13
bolton.nick <bolton.nick@784e0458-0e37-0410-a381-656f3defdab0>
2007-11-03 12:15:56 +0000

    reverting release

--- tags/toro-portal-1.0.0/installer/src/main/resources/windows-icons/installIcons-docs.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-REM this batch file exists so that the parameters of this line are NOT quoted
-rundll32 setupapi,InstallHinfSection DefaultInstall 128 @installDir@\windows-icons\installIcons-docs.inf
